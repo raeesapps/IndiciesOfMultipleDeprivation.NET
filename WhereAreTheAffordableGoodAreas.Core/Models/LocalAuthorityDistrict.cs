91b8b23c257f9fb653e232753cdaeefebf0b5b7f
raeesapps <raees.aamir@yandex.com>
2020-05-29 13:34:53 +0100

    Made LocalAuthorityDistrict equatable

--- WhereAreTheAffordableGoodAreas.Core/Models/LocalAuthorityDistrict.cs
+++ WhereAreTheAffordableGoodAreas.Core/Models/LocalAuthorityDistrict.cs
@@ -1,7 +1,29 @@
+using System;
+
 namespace WhereAreTheAffordableGoodAreas.Models
 {
-    public class LocalAuthorityDistrict
+    public class LocalAuthorityDistrict : IEquatable<LocalAuthorityDistrict>
     {
+        public bool Equals(LocalAuthorityDistrict other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Code == other.Code && Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((LocalAuthorityDistrict) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Code, Name);
+        }
+
         public string Code { get; set; }
         public string Name { get; set; }
     }
