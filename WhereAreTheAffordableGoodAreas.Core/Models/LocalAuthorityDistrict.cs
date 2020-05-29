b45e9e4dbfdb39f85e99c9694f8147c751eb3b0b
raeesapps <raees.aamir@yandex.com>
2020-05-29 18:27:53 +0100

    LocalAuthorityDistrict is hashable now

--- WhereAreTheAffordableGoodAreas.Core/Models/LocalAuthorityDistrict.cs
+++ WhereAreTheAffordableGoodAreas.Core/Models/LocalAuthorityDistrict.cs
@@ -1,9 +1,30 @@
+using System;
 using System.Text.Json;
 
 namespace WhereAreTheAffordableGoodAreas.Models
 {
-    public class LocalAuthorityDistrict
+    public class LocalAuthorityDistrict : IEquatable<LocalAuthorityDistrict>
     {
+        public bool Equals(LocalAuthorityDistrict other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Code == other.Code && Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((LocalAuthorityDistrict) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Code, Name);
+        }
+
         public string Code { get; set; }
         public string Name { get; set; }
         
