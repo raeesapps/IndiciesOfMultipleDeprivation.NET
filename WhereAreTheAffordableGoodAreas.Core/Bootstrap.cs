cef5e4ee8478c98eee6c93e0e2c2d873b3f7db47
raeesapps <raees.aamir@yandex.com>
2020-05-29 15:16:33 +0100

    Added it to bootstrap

--- WhereAreTheAffordableGoodAreas.Core/Bootstrap.cs
+++ WhereAreTheAffordableGoodAreas.Core/Bootstrap.cs
@@ -1,23 +1,32 @@
+using System;
+using WhereAreTheAffordableGoodAreas.Models;
 using WhereAreTheAffordableGoodAreas.Parser;
 
 namespace WhereAreTheAffordableGoodAreas
 {
     public class Bootstrap
     {
-        private readonly LowerLayerSuperOutputAreaParser _lowerLayerSuperOutputAreaParser;
+        private readonly ILinearParser<LowerLayerSuperOutputArea> _lowerLayerSuperOutputAreaParser;
+        private readonly IKeyValueParser<string, string> _lowerLayerSupportOutputAreaToWardParser;
 
-        public Bootstrap(string path)
+        public Bootstrap(string lowerLayerSuperOutputAreaPath, string lowerLayerSuperOutputAreaToWardPath)
         {
-            _lowerLayerSuperOutputAreaParser = new LowerLayerSuperOutputAreaParser(path);
+            _lowerLayerSuperOutputAreaParser = new LowerLayerSuperOutputAreaParser(lowerLayerSuperOutputAreaPath);
+            _lowerLayerSupportOutputAreaToWardParser = new LowerLayerSuperOutputAreaToWardParser(lowerLayerSuperOutputAreaToWardPath);
         }
 
         public void Start()
         {
             var lowerLayerSuperOutputAreas = _lowerLayerSuperOutputAreaParser.Parse();
-            
             foreach (var lowerLayerSuperOutputArea in lowerLayerSuperOutputAreas)
             {
                 //Console.WriteLine(lowerLayerSuperOutputArea);
+            }
+
+            var lowerLayerSupportOutputAreasToWards = _lowerLayerSupportOutputAreaToWardParser.KeyValueParse();
+            foreach (var kv in lowerLayerSupportOutputAreasToWards)
+            {
+                Console.WriteLine(kv);
             }
         }
     }
