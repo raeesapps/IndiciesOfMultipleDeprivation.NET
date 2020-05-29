2b496ca0e3b2871f0b8f880f6e5d0a3d2ef8adf2
raeesapps <raees.aamir@yandex.com>
2020-05-29 13:11:15 +0100

    init commit

---
+++ WhereAreTheAffordableGoodAreas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhereAreTheAffordableGoodAreas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
