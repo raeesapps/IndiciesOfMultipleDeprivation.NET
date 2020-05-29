49a94122697cc9e5167e0f057fed4d7d4498f93f
raeesapps <raees.aamir@yandex.com>
2020-05-29 16:27:45 +0100

    Forgot to commit bootstrap

--- WhereAreTheAffordableGoodAreas.Core/Program.cs
+++ WhereAreTheAffordableGoodAreas.Core/Program.cs
@@ -6,7 +6,7 @@ namespace WhereAreTheAffordableGoodAreas
     {
         static void Main(string[] args)
         {
-            var bootstrap = new Bootstrap("//Users/raees/Documents/area_data/imd.csv", "//Users/raees/Documents/area_data/llsoatoward.csv");
+            var bootstrap = new Bootstrap("//Users/raees/Documents/area_data/imd.csv", "//Users/raees/Documents/area_data/houseprices.csv", "//Users/raees/Documents/area_data/llsoatoward.csv");
             bootstrap.Start();
         }
     }
