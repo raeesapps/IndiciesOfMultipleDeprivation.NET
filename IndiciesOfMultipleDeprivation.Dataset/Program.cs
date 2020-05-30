78dbf1c6f7694087922e1ee1e02a9366677dd9f2
raeesapps <raees.aamir@yandex.com>
2020-05-30 15:11:35 +0100

    Change accessor

--- IndiciesOfMultipleDeprivation.Dataset/Program.cs
+++ IndiciesOfMultipleDeprivation.Dataset/Program.cs
@@ -2,7 +2,7 @@ namespace IndiciesOfMultipleDeprivation
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var bootstrap = new Bootstrap("//Users/raees/Documents/area_data/imd.csv", "//Users/raees/Documents/area_data/houseprices.csv", "//Users/raees/Documents/area_data/llsoatoward.csv");
             bootstrap.Start();
