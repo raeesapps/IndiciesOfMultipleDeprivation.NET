53b5d8abca10c6dce315befe057ce045774403bd
raeesapps <raees.aamir@yandex.com>
2020-05-30 19:14:21 +0100

    Implemented model for containing the whole dataset

--- IndiciesOfMultipleDeprivation.Dataset/Models/Dataset.cs
+++ IndiciesOfMultipleDeprivation.Dataset/Models/Dataset.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+
 namespace IndiciesOfMultipleDeprivation.Models
 {
     public class Dataset
     {
-        
+        public IList<LowerLayerSuperOutputArea> LowerLayerSuperOutputAreas { get; set; }
+        public IDictionary<string, string> LowerLayerSuperOutputAreaCodeToWardCode { get; set; }
+        public IList<HousePrice> HousePrices { get; set; }
     }
 }
