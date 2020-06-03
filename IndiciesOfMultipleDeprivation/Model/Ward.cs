d3c8a424b4c4933363d0d7d22bffc038f52b5279
raeesapps <raees.aamir@yandex.com>
2020-06-03 18:47:50 +0100

    Made the two DAOs equatable for testing

--- IndiciesOfMultipleDeprivation/Model/Ward.cs
+++ IndiciesOfMultipleDeprivation/Model/Ward.cs
@@ -1,9 +1,30 @@
+using System;
 using System.Text.Json;
 
 namespace IndiciesOfMultipleDeprivation.Model
 {
-    public class Ward
+    public class Ward : IEquatable<Ward>
     {
+        public bool Equals(Ward other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Name == other.Name && Code == other.Code;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Ward) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Code);
+        }
+
         public string Name { get; set; }
         public string Code { get; set; }
         
