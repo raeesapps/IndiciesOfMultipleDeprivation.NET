3c9c5a19aa4d2c507395fc60a04a88e955f8762c
raeesapps <raees.aamir@yandex.com>
2020-06-24 16:21:29 +0100

    Implemented an endpoint for retrieving affluent wards

---
+++ IndiciesOfMultipleDeprivation.Api/Controllers/WardController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IndiciesOfMultipleDeprivation.Model;
+using IndiciesOfMultipleDeprivation.Query;
+using IndiciesOfMultipleDeprivation.Query.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IndiciesOfMultipleDeprivation.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WardController : ControllerBase
+    {
+        private readonly IDatasetProvider _datasetProvider;
+        private readonly IQueryChain _queryChain;
+        private readonly IFilterLowerLayerSuperOutputAreasByDecileQuery _filterLowerLayerSuperOutputAreasByDecileQuery;
+        private readonly IGetHousePricesOfLowerLayerSuperOutputAreasQuery _getHousePricesOfLowerLayerSuperOutputAreasQuery;
+        private readonly ISortHousePricesByAverageHousePricesQuery _sortHousePricesByAverageHousePricesQuery;
+
+        public WardController(IDatasetProvider datasetProvider,
+            IQueryChain queryChain,
+            IFilterLowerLayerSuperOutputAreasByDecileQuery filterLowerLayerSuperOutputAreasByDecileQuery,
+            IGetHousePricesOfLowerLayerSuperOutputAreasQuery getHousePricesOfLowerLayerSuperOutputAreasQuery,
+            ISortHousePricesByAverageHousePricesQuery sortHousePricesByAverageHousePricesQuery)
+        {
+            _datasetProvider = datasetProvider;
+            _queryChain = queryChain;
+            _filterLowerLayerSuperOutputAreasByDecileQuery = filterLowerLayerSuperOutputAreasByDecileQuery;
+            _getHousePricesOfLowerLayerSuperOutputAreasQuery = getHousePricesOfLowerLayerSuperOutputAreasQuery;
+            _sortHousePricesByAverageHousePricesQuery = sortHousePricesByAverageHousePricesQuery;
+        }
+
+        [HttpGet]
+        public IEnumerable<HousePrice> Let()
+        {
+            _queryChain.Queries.Add(_filterLowerLayerSuperOutputAreasByDecileQuery);
+            _queryChain.Queries.Add(_getHousePricesOfLowerLayerSuperOutputAreasQuery);
+            _queryChain.Queries.Add(_sortHousePricesByAverageHousePricesQuery);
+
+            var result =
+                _queryChain
+                    .Execute(null, _datasetProvider.Dataset)
+                    .Cast<HousePrice>()
+                    .ToList();
+
+            return result;
+        }
+    }
+}
